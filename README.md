0499bf8f5d6c10d48cbcc98c66b0bcbed1c3e5eb
shivenaggarwal <97015991+shivenaggarwal@users.noreply.github.com>
2023-07-17 10:51:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
---
+++ script.js
@@ -0,0 +1,109 @@
+"use strict";
+
+const billInput = document.querySelector("#bill-input");
+const customTipInput = document.querySelector(".custom-tip-input");
+const numberOfPeopleInput = document.querySelector("#number-of-people-input");
+const tipAmountValue = document.querySelector(".tip-amount-value");
+const totalValue = document.querySelector(".total-value");
+const btnReset = document.querySelector(".btn-reset");
+const selectTipBtns = document.querySelectorAll(".select-tip-percentage");
+
+btnReset.addEventListener("click", (event) => {
+  billInput.value = "0";
+  customTipInput.value = "";
+  numberOfPeopleInput.value = "1";
+  tipAmountValue.innerHTML = "$0";
+  totalValue.innerHTML = "$0";
+
+
+  selectTipBtns.forEach((tipBtn) => {
+    tipBtn.classList.remove("active");
+  });
+
+
+  selectTipBtns[2].classList.add("active");
+});
+
+
+selectTipBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    selectTipBtns.forEach((tipBtn) => {
+      tipBtn.classList.remove("active");
+    });
+
+    if (event.target.classList.contains("custom-tip-input")) {
+      event.target.parentElement.classList.add("active");
+    } else {
+      event.target.classList.add("active");
+    }
+
+    calculateTip();
+  });
+});
+
+const calculateTip = () => {
+  const billValue = parseFloat(billInput.value);
+  const numberOfPeople = parseFloat(numberOfPeopleInput.value);
+  const customTipActive = document.querySelector(".select-tip-custom.active");
+  let tipPercentage = parseInt(
+    document.querySelector(".select-tip-percentage.active").dataset.percentage
+  );
+
+  if (customTipActive) {
+    tipPercentage = parseFloat(
+      document.querySelector(".custom-tip-input").value
+    );
+  }
+
+  const totalAmount = parseFloat((tipPercentage / 100) * billValue).toFixed(2);
+  const tipAmount = parseFloat(totalAmount / numberOfPeople).toFixed(2);
+  const actualTotalAmount = parseFloat(
+    (billValue + parseFloat(totalAmount)) / numberOfPeople
+  ).toFixed(2);
+
+  tipAmountValue.innerHTML = `$${tipAmount}`;
+  totalValue.innerHTML = `$${actualTotalAmount}`;
+};
+
+const isNumber = (value) => {
+  if (
+    value === "Backspace" ||
+    value === "ArrowLeft" ||
+    value === "ArrowRight" ||
+    value === "."
+  ) {
+    return true;
+  }
+
+  const regex = /^[0-9]+$/;
+
+  return regex.test(value);
+};
+
+
+billInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+customTipInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+numberOfPeopleInput.addEventListener("keyup", (event) => {
+  calculateTip();
+});
+
+
+billInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
+customTipInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
+numberOfPeopleInput.addEventListener("keydown", (event) => {
+  if (!isNumber(event.key)) {
+    event.preventDefault();
+  }
+});
